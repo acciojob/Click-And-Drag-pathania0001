68c0406ea92b761afa4ebec7de44cdf546af87ae
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-19 11:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your code here.
+
+const items  = document.querySelectorAll(".item")
+items.forEach((item)=>{
+item.draggable = true;
+	//console.log(item.classList[1])
+item.ondragstart = (e)=>{
+  e.dataTransfer.setData("data",item.classList[1]);
+	console.log(e.dataTransfer.getData("data"));
+}
+	item.ondragover=(e)=>{
+		e.preventDefault();
+	} 
+item.ondrop = (e)=>{
+	console.log(e);
+	const data = document.querySelector(`.${e.dataTransfer.getData("data")}`);
+  item.insertAdjacentElement("beforebegin",data)
+}
+})
